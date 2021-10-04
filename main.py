292302d73e3a687319356279b84fc106a86c6c78
makefrontend <69586129+makefrontend@users.noreply.github.com>
2021-10-05 00:08:48 +0300

    hotfix

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import requests
 
 
 path = './books'
-with open('filestreem.txt') as file_streem:
+with open('filestreem.txt') as books_url:
     os.makedirs(path, exist_ok=True)
     os.chdir(path)
-    for index, line_streem in enumerate(file_streem):
-        response = requests.get(line_streem)
+    for index, book_url in enumerate(books_url):
+        response = requests.get(book_url)
         response.raise_for_status() 
         filename = f'id{index+1}.txt'
         with open(filename, 'wb') as file:
