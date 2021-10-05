f3231572a3b191653cbe190a1a236bce52be2e94
makefrontend <69586129+makefrontend@users.noreply.github.com>
2021-10-06 00:50:03 +0300

    download book covers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup	
@@ -21,6 +22,17 @@ def download_txt(url, filename, folder='books/'):
     return filepath
 
 
+def download_img(url, filename, folder='image/'):
+    os.makedirs(folder, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+
+    filepath = os.path.join(folder, sanitize_filename(filename))
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+    return filepath
+
+
 with open('filestreem.txt') as books_url:
     for book_url in books_url:
         response = requests.get(book_url)
@@ -35,9 +47,12 @@ with open('filestreem.txt') as books_url:
         book_title = book_data[0].strip()
         book_author = book_data[1].strip()
         book_link_id = book_url.split('/')[-2].strip('b')
+        book_img_id = soup.select_one('div.bookimage img')['src']
         book_txt_link = f'https://tululu.org/txt.php?id={book_link_id}'
+        book_img_link = urljoin('https://tululu.org/', book_img_id)
         book_path = download_txt(book_txt_link, book_title)
+        img_path = download_img(book_img_link, book_img_id)
         print('Заголовок: ', book_title)
         print('Автор: ', book_author)
-        print('Путь до книги: ', book_path)
+        print('Картинка книги: ', book_img_link)
 print('Парсинг завершен!')
